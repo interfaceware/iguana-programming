c7b7259bedc3e304d2c487d5193a5bd6720ddd8f
admin <paul.gannon@interfaceware.com>
2016-02-25 13:57:47 +0000

    Export: Got rid of baseurl module - not really needed.

--- Dependency_report-FromHTTP-xfMKwcW0UqhNqC/main.lua
+++ Dependency_report-FromHTTP-xfMKwcW0UqhNqC/main.lua
@@ -1,7 +1,6 @@
 -- The main function is the first function called from Iguana.
 -- The Data argument will contain the message to be processed.
 require 'depend'
-require 'baseurl'
 require 'search'
 
 function main(Data)
@@ -10,7 +9,8 @@ function main(Data)
    
    local R = net.http.parseRequest{data=Data}
    
-   BaseUrl = baseurl.url(R)
+   BaseUrl = 'http://'..R.headers.Host:split(':')[1]..':'..iguana.webInfo().web_config.port..'/'
+   trace(BaseUrl)
    
    if R.params.search then
       Report = SearchReport(R.params.search, TChanMap, MList, BaseUrl)
